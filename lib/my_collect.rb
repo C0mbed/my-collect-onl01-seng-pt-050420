549202f9f58c68cd77f4462836c421c9ed8ae2a0
Matt Farncombe <matt@farncombe.co>
2020-05-07 20:41:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,18 @@
-my_collect(collection) do |lang|
-  lang.upcase
+def my_collect(collection)
+  def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 end
 
