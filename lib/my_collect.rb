86053beb1a267b83a92dbd4970485642e6b48cb9
Matt Farncombe <matt@farncombe.co>
2020-05-07 20:44:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,9 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.length
-    new_collection << yield(collection[i])
+    full_name =  yield(collection[i])
+    split_name = full_name.split(" ")
+    new_collection << split_name[0]
     i += 1
   end
   collection
